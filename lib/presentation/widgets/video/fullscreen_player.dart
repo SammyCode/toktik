ab498e89f803da3f6a5a94cc35c6bacc5dbace81
Sam <sam.tepec.dev@gmail.com>
2024-04-16 18:18:36 -0600

    Implement pause

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -40,22 +40,32 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
         if (snapshot.connectionState != ConnectionState.done) {
           return const Center(child: CircularProgressIndicator(strokeWidth: 2));
         }
-        return AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: Stack(
-              children: [
-                VideoPlayer(controller),
+        return GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying) {
+              controller.pause();
+              return;
+            }
 
-                // gradiente
+            controller.play();
+          },
+          child: AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(
+                children: [
+                  VideoPlayer(controller),
 
-                //texto
+                  // gradiente
 
-                Positioned(
-                    bottom: 50,
-                    left: 20,
-                    child: _VideoCaption(caption: widget.caption))
-              ],
-            ));
+                  //texto
+
+                  Positioned(
+                      bottom: 50,
+                      left: 20,
+                      child: _VideoCaption(caption: widget.caption))
+                ],
+              )),
+        );
       },
     );
   }
