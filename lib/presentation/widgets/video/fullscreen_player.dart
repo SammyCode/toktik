834e6a525b7ddcb15903c6bfa545ef465e3386fe
Sam <sam.tepec.dev@gmail.com>
2024-04-16 18:44:19 -0600

    Implement gradient

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -56,6 +57,7 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
                   VideoPlayer(controller),
 
                   // gradiente
+                  VideoBackground(stops: const [0.8, 1.0]),
 
                   //texto
 
