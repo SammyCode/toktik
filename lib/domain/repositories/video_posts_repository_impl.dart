0c1140c4612a998ef50b185143a1a7065110437d
Sam <sam.tepec.dev@gmail.com>
2024-04-19 16:47:32 -0600

    Implements datasources & repositories

---
+++ lib/domain/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,21 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+
+class VideoPostsRepository implements VideoPostRepository {
+  final VideoPostDatasource videosDatasource;
+
+  VideoPostsRepository({
+    required this.videosDatasource,
+  });
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+}
